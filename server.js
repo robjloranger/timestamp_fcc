f4af2f45c61458da4ebbdebccc972c0ac344973f
robjloranger <robjloranger@gmail.com>
2016-02-26 16:29:25 -0800

    updated urls to heroku for now

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(function(request,response,next){
   if(request.path === "/" || request.path === "/api"){
     next();
   }else{
-    response.status(404).json("Path requested does not exist. Please visit http://microtime.robloranger.ca for more information.");
+    response.status(404).json("Path requested does not exist. Please visit http://robsfcctimestamps.herokuapp.com/ for more information.");
   }
 });
 app.get('/api', function(request,response){
@@ -19,7 +19,7 @@ app.get('/api', function(request,response){
       response.send(result);
     }
   }else{
-    response.status(400).json("Bad request, the string provided was not a valid natural date string or no string was provided. Please refer to the api documentation at http://microtime.robloranger.ca");
+    response.status(400).json("Bad request, the string provided was not a valid natural date string or no string was provided. Please refer to the api documentation at http://robsfcctimestamps.herokuapp.com/");
   }
 });
 
